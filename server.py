449ecbd6a578e2c1ca19058945e4b00b272db3ad
Jordan Singleton <jordanwcsingleton@gmail.com>
2014-04-06 02:31:33 -0400

    fix

--- server.py
+++ server.py
@@ -18,40 +18,51 @@ class Server:
     self.socket.listen(1)
     self.socket.settimeout(0)
     self.connected = 0
+    self.conn = None
  
   def start(self):
+    print "XPWN start"
     while(True):
       try:
         self.accept()
+        print "XPWN: sockname: " + str(self.socket.getsockname())
         self.handshake()
         self.loop()
-      except Exception socket.error: pass
+      except  socket.error: pass
       self.port = 0
-      self.conn.close()
+      if self.conn:
+        self.conn.close()
       self.conn_addr = None
       self.conn = None
 
   def accept(self):
+    print "XPWN accept"
     self.conn, self.conn_addr = self.socket.accept()
 
   def handshake(self):
+    print "XPWN handshake"
     try:
       data = self.conn.recv(512)
-      print data
-      self.port = data.split()[1]
-      self.conn.send("ACK ")#%d" % self.port)
+      print "XPWN: " + str(data)
+      self.port = int(data.split()[1])
+      self.conn.send("XPWN: ACK ")#%d" % self.port)
       self.connected = 1
-    except Exception socket.error:
-      print "handshake error, closing connection"
+    except  socket.error, TypeError:
+      print "XPWN: handshake error, closing connection"
       self.conn.close()
 
   def loop(self):
+    print "XPWN loop"
     self.conn.settimeout(200)
     while(self.connected):
       try:
         #TODO: might cause an infinite loop
         data = self.conn.recv(512)
-      except Exception socket.error: continue
+      except  socket.error: continue
+      if len(data) == 0:
+        print "XPWN DATA len 0 and error not caught"
+        continue
+      print "XPWN Data: " + data
       switch = data[0]
       if switch == "q": self.connected = 0
       elif switch == "s": self.stream()
@@ -60,22 +71,27 @@ class Server:
       elif switch == "y": self.youtube_connect(data[1:]) 
 
   def stream(self):
-    self.vlc_connect((self.conn_addr, self.port))
+    print "XPWN stream"
+    self.vlc_connect(self.conn_addr, self.port)
 
-  def vlc_connect((address, port)):
+  def vlc_connect(self, address, port):
+    print "XPWN vlc_connect"
     command = 'XBMC.PlayMedia(http://' + str(address[0]) + ':' + str(port) + ')'
     print command
     xbmc.executebuiltin(command)
 
   def youtube_connect(self, vid):
+    print "XPWN youtube_connect"
     command = 'XBMC.PlayMedia(plugin://plugin.video.youtube/?path=/root/video&action=play_video&videoid=' + vid + ')'
     xbmc.executebuiltin(command)
 
   def stop(self):
-    command = "XBMC.PlayerControl(Stop)"
+    print "XPWN stop"
+    command = "XBMC.PlayerControl(self, Stop)"
     xbmc.executebuiltin(command)
 
   def play(self):
-    command = "XBMC.PlayerControl(Play)"
+    print "XPWN play"
+    command = "XBMC.PlayerControl(self, Play)"
     xbmc.executebuiltin(command)
     
